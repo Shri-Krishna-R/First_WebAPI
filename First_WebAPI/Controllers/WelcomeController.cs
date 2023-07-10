d7f28054d80997371cc4f54c2e4c008b75ed1d02
Administrator <Administrator@84D15FFC57C85FF>
2023-07-10 10:34:07 +0530

    Check in 1

---
+++ First_WebAPI/Controllers/WelcomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace First_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WelcomeController : ControllerBase
+    {
+        [Route("/Welcome")]
+        [HttpGet]
+        public IActionResult Greet()
+        {
+            return Ok("Testing the Greet Option");
+        }
+        [Route("/Welcome/{name}")]
+        [HttpGet]
+        public IActionResult Greet(string name)
+        {
+            string message = "Hello " + name + ". Welcome to test API";
+            return Ok(message);
+        }
+    }
+}
